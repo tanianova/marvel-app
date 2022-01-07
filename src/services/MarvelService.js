0d73ad0ef4538930ba28481363e5b28161ca9413
Tatiana Tikhonova <tanianova@yandex.ru>
2022-01-07 11:23:32 +0300

    get random character from API

--- src/services/MarvelService.js
+++ src/services/MarvelService.js
@@ -1,6 +1,6 @@
 class MarvelService {
   _apiBase = "https://gateway.marvel.com:443/v1/public/";
-  _apiKey = 'apikey=ae8ec0e15efe5dafc93642163047b6d1'
+  _apiKey = "apikey=ae8ec0e15efe5dafc93642163047b6d1";
 
   getResource = async (url) => {
     let res = await fetch(url);
@@ -11,15 +11,29 @@ class MarvelService {
     return await res.json();
   };
 
-  getAllCharacters = () => {
-    return this.getResource(
+  getAllCharacters = async () => {
+    const res = await this.getResource(
       `${this._apiBase}characters?limit=9&offset=210&${this._apiKey}`
     );
+    return res.data.results.map(this._transformCharacter);
   };
-  getCharacter = (id) => {
-    return this.getResource(
+  getCharacter = async (id) => {
+    const res = await this.getResource(
       `${this._apiBase}characters/${id}?${this._apiKey}`
     );
+    return this._transformCharacter(res.data.results[0]);
+  };
+
+  _transformCharacter = (char) => {
+    return {
+      name: char.name,
+      description: char.description
+        ? `${char.description.slice(0, 210)}...`
+        : "There is no description for this character",
+      thumbnail: char.thumbnail.path + "." + char.thumbnail.extension,
+      homepage: char.urls[0].url,
+      wiki: char.urls[1].url,
+    };
   };
 }
 
